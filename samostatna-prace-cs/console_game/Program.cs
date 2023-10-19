708e0fac493c08d68f61f0f0da6c344bc559bad9
ondradoksy <ondradoksy@ondradoksy.com>
2023-10-19 20:25:51 +0200

    Added restart and highscore

--- samostatna-prace-cs/console_game/Program.cs
+++ samostatna-prace-cs/console_game/Program.cs
@@ -15,48 +15,57 @@ namespace console_game
             Vector minBounds = new Vector(0, 0);
             Vector maxBounds = new Vector(_sceneWidth, _sceneHeight);
             Scene scene = new Scene(_sceneWidth, _sceneHeight);
-            Player player = new Player(new Vector(3, 0)) { MinBounds = minBounds, MaxBounds = maxBounds };
+            int highScore = 0;
 
-            GameObject obstacle1 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
-            Action onObstacle1Destroy = null;
-            onObstacle1Destroy = () =>
-                {
-                    obstacle1 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
-                    obstacle1.OnDestroy = onObstacle1Destroy;
-                };
-            obstacle1.OnDestroy = onObstacle1Destroy;
-
-            GameObject obstacle2 = RandomObstacle(minBounds, maxBounds, (int)(_sceneWidth * 1.5));
-            Action onObstacle2Destroy = null;
-            onObstacle2Destroy = () =>
-                {
-                    obstacle2 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
-                    obstacle2.OnDestroy = onObstacle2Destroy;
-                };
-            obstacle2.OnDestroy = onObstacle2Destroy;
-
-            TextBox scoreCounter = new TextBox(new Vector(_sceneWidth - 15, 0), 15, 3);
-            int score = 0;
+            TextBox highScoreCounter = new TextBox(new Vector(_sceneWidth - 20, 0), 20, 3);
+            TextBox scoreCounter = new TextBox(new Vector(_sceneWidth - 15, 2), 15, 3);
 
             while (true)
             {
-                ProcessInput();
-                if (_jump > 0) player.Jump();
-                else if (_crouch > 0) player.Crouch();
-                else player.StandUp();
+                highScoreCounter.UpdateText($"High Score: {highScore}");
 
-                player.Move();
-                obstacle1.Move();
-                obstacle2.Move();
+                Player player = new Player(new Vector(3, 0)) { MinBounds = minBounds, MaxBounds = maxBounds };
 
-                scoreCounter.UpdateText($"Score: {score}");
-                scene.Draw(player, obstacle1, obstacle2, scoreCounter);
+                GameObject obstacle1 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
+                Action onObstacle1Destroy = null;
+                onObstacle1Destroy = () =>
+                    {
+                        obstacle1 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
+                        obstacle1.OnDestroy = onObstacle1Destroy;
+                    };
+                obstacle1.OnDestroy = onObstacle1Destroy;
 
-                if (player.CollideWith(obstacle1.Collider) || player.CollideWith(obstacle2.Collider)) break;
-                score++;
-                Thread.Sleep(100);
+                GameObject obstacle2 = RandomObstacle(minBounds, maxBounds, (int)(_sceneWidth * 1.5));
+                Action onObstacle2Destroy = null;
+                onObstacle2Destroy = () =>
+                    {
+                        obstacle2 = RandomObstacle(minBounds, maxBounds, _sceneWidth);
+                        obstacle2.OnDestroy = onObstacle2Destroy;
+                    };
+                obstacle2.OnDestroy = onObstacle2Destroy;
+
+                int score = 0;
+
+                while (true)
+                {
+                    ProcessInput();
+                    if (_jump > 0) player.Jump();
+                    else if (_crouch > 0) player.Crouch();
+                    else player.StandUp();
+
+                    player.Move();
+                    obstacle1.Move();
+                    obstacle2.Move();
+
+                    scoreCounter.UpdateText($"Score: {score}");
+                    scene.Draw(player, obstacle1, obstacle2, highScoreCounter, scoreCounter);
+
+                    if (player.CollideWith(obstacle1.Collider) || player.CollideWith(obstacle2.Collider)) break;
+                    score++;
+                    Thread.Sleep(100);
+                }
+                if (score > highScore) highScore = score;
             }
-
         }
         private static void ProcessInput()
         {
